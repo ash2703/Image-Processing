16e813809835787975e04b7b11f92dd239776b72
Aayush  Singh <42978599+ash2703@users.noreply.github.com>
2020-10-06 16:05:24 +0530

    Added mean of histogram

--- mean-variance-energy.py
+++ mean-variance-energy.py
@@ -1,5 +1,12 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt 
+
+def get_hist(img):
+    histogram = np.zeros(256)
+    for pixel in img:
+        histogram[pixel] += 1
+    return histogram
 
 img = cv2.imread('Images/lena.jpeg',0)
 
@@ -10,3 +17,12 @@ energy = np.sum(np.square(img))/(img.shape[0] * img.shape[1])
 print("calculated  mean: {}, variance: {}, energy: {}".format(mean,variance,energy))
 
 print("check numpy mean: {}, variance: {}, energy: {}".format(np.mean(img),np.var(img),energy))
+
+hist = get_hist(img.flat)
+p_x = hist/np.sum(hist)  #calculate probability
+hist_mean = 0
+for i in range(len(p_x)):
+    hist_mean = hist_mean + i*p_x[i]   #x*p(x)
+print(hist_mean)
+plt.plot(hist)
+plt.show()
